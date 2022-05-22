3f004513873af5b037ab87819c200e7db40a1316
João Victor Torres Borges <joao.victor.tb@gmail.com>
2022-05-22 10:27:03 -0300

    implement sense and slam functions

--- robot_class_euclidian_distance.py
+++ robot_class_euclidian_distance.py
@@ -93,9 +93,9 @@ class robot:
             ## 3. If either of the distances, dx or dy, fall outside of the internal var, measurement_range
             ##    then we cannot record them; if they do fall in the range, then add them to the measurements list
             ##    as list.append([index, dx, dy]), this format is important for data creation done later
-            if abs(dx) > self.measurement_range and abs(dy) > self.measurement_range:
-                continue
-            measurements.append([idx, dx, dy])
+            dist = np.sqrt(dx**2 + dy**2)
+            if dist < self.measurement_range and self.measurement_range != -1:
+            	measurements.append([idx, dx, dy])
         ## TODO: return the final, complete list of measurements
         return measurements
 
